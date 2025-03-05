855c11a7ab9d2517fb8869b99bf32d560c464f99
Jörg <jbantin@gmx.de>
2025-03-05 15:35:29 +0100

    10statt12

--- libs/crypto.ts
+++ libs/crypto.ts
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt";
 
 export const hash = async (password: string) => {
-  return bcrypt.hash(password, 12);
+  return bcrypt.hash(password, 10);
 };
 export const compare = async (password: string, hashedPassword: string) => {
   return bcrypt.compare(password, hashedPassword);
