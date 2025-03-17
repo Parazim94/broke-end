c1fd3178db29af716177957f9c48cb788ce3531e
Jörg <jbantin@gmx.de>
2025-03-17 11:49:32 +0100

    google auth added

--- models/User.ts
+++ models/User.ts
@@ -17,6 +17,7 @@ const userSchema = new Schema(
   {
     userName: String,
     email: { type: String, required: true, unique: true },
+    method: { type: String, default: "email" },
     oldEmail: { type: String },
     password: { type: String, require: true },
     age: { type: Number, required: true },
--- routes/authRouter.ts
+++ routes/authRouter.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import session from "express-session";
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { User } from "../models/User";
 import { DeletedToken } from "../models/DeletedToken";
 import { hash, compare } from "../libs/crypto";
@@ -9,8 +12,72 @@ import { Order } from "../models/Orders";
 import sendVerificationEmail from "../libs/sendVerificationEmail";
 import createStandardResponse from "../libs/createStandardResponse";
 
-
 const router = express.Router();
+const MY_SECRET_KEY = process.env.MY_SECRET_KEY || "defaultSecret";
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || "";
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || "";
+const GOOGLE_CALLBACK_URL = process.env.GOOGLE_CALLBACK_URL || "";
+router.use(
+  session({
+    secret: MY_SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+router.use(passport.initialize());
+router.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user as typeof User);
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: GOOGLE_CALLBACK_URL,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      return done(null, profile);
+    }
+  )
+);
+
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  async (req, res, next) => {
+    // Successful authentication
+    const email = (req.user as any).emails[0].value;
+    let user = await User.findOne({ email });
+    if (!user) {
+      const userName = req.body.displayName;
+
+      user = await User.create({
+        userName,
+        email,
+        age: 66,
+        method: "google",
+        hashedPW: "Googlekowski",
+      });
+    }
+    const jwt = createJwt(email);
+    const userObject = user.toJSON();
+    const orders = await Order.find({ user_id: user._id });
+    res.status(200).send({ ...userObject, token: jwt, orders });
+  }
+);
 
 router.post("/register", async (req, res, next) => {
   try {
